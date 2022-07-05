5dcf6dede097422e0f60e0d7a67720286c8157fd
senjenathaniel <topsailcashew@icloud.com>
2022-07-05 07:19:07 +0300

    App working: next up create structure to api

---
+++ api/controllers/PostsController.js
@@ -0,0 +1,41 @@
+/**
+ * PostsController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  // friendlyName: "Show blog posts",
+
+  // description: "Fetches one or all blog posts from the database",
+
+  // inputs: {
+  //   postId: {
+  //     description: "Id for one post"
+  //   },
+
+  //   type: 'number'
+
+  // },
+
+  // exits: {
+  //   success: {
+  //     responseType: 'view',
+  //     viewTemplatePath: 'page/posts',
+  //   }
+  // },
+
+  // fn: async function ({
+  //   postId
+  // }) {
+  //   var post = await Post.findOne({
+  //     id: postId
+  //   });
+
+  //   return {
+  //     postTitle: post.title
+  //   };
+  // }
+
+};
